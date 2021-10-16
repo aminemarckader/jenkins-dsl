ed7baaf097fb99511fb64f3fbdd2c85638f79963
Amine Kader <aminemarckader@gmail.com>
2021-10-16 16:13:04 +0100

    updated

--- nodejs.groovy
+++ nodejs.groovy
@@ -1,20 +1,39 @@
-job('Inventory Server'){
+// job('Inventory Server'){
+//     scm {
+//         git('https://github.com/aminemarckader/inventory-server.git') {
+//             node /gitConfigName('aminemarckader')
+//             node /gitConfigEmail('aminemarckader@gmail.com')
+//         }
+//     }
+
+//     triggers {
+//         scm('H/5 * * * *')
+//     }
+
+//     wrappers {
+//         nodejs('nodejs')
+//     }
+
+//     steps {
+//         shell('npm i')
+//     }
+// }
+
+job('node-express') {
     scm {
-        git('https://github.com/aminemarckader/inventory-server.git') {
-            node /gitConfigName('aminemarckader')
-            node /gitConfigEmail('aminemarckader@gmail.com')
+        git('https://github.com/lbounif/deploy-example.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('Lynda Bounif')
+            node / gitConfigEmail('l_bounif@esi.dz')
         }
     }
-
     triggers {
         scm('H/5 * * * *')
     }
-
     wrappers {
-        nodejs('nodejs')
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
-
     steps {
-        shell('npm i')
+        shell("npm install")
     }
 }
